ce569a0fcd54a5bd176b960301d07e3fd26aeece
Vyacheslav <Vyacheslav@DESKTOP-JMNADMM>
2023-05-01 16:00:00 +0300

    add complited part 3

---
+++ Tutorial_1_of_8/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial_1_of_8.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
